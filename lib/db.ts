6d9721e5f0448f9a4eed7f9c5044903d1d0c5537
ahmed hassan <you@example.com>
2026-01-03 08:44:26 +0500

    dinal

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI||"mongodb+srv://ah770643:2H3IHP4cvAsXzhW8@cluster0.bdbqw.mongodb.net/Fitnesswebsite?retryWrites=true&w=majority&appName=Cluster0"
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable in .env.local');
@@ -32,7 +32,7 @@ async function dbConnect() {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
     };
-
+//@ts-ignore
     cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
       return mongoose;
     });
